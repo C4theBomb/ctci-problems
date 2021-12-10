eed3bed068af164e1cdc0d309270493be0758dd1
C4 Patino <c4patino@gmail.com>
2021-12-10 10:36:55 -0600

    Created tests for MatrixRotation problem

---
+++ src/test/java/com/C4theBomb/datastructures/arraysandstrings/MatrixRotationTest.java
@@ -0,0 +1,23 @@
+package com.C4theBomb.datastructures.arraysandstrings;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class MatrixRotationTest {
+    @Test
+    @DisplayName("rotateMatrix (3(m) x 3(n)) - n x m")
+    void rotate3By3Matrix() {
+        int[][] initial = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] rotated = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+        assertArrayEquals(rotated, MatrixRotation.rotateMatrix(initial));
+    }
+
+    @Test
+    @DisplayName("rotateMatrix (4(m) x 4(n)) - n x m")
+    void rotate4By4Matrix() {
+        int[][] initial = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] rotated = {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
+        assertArrayEquals(rotated, MatrixRotation.rotateMatrix(initial));
+    }
+}

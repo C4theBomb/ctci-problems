a3b0b3f864c315178451d7d0a0b2dbfd649bb755
C4 Patino <c4patino@gmail.com>
2021-12-10 14:03:46 -0600

    Completed MatrixRotation problem

---
+++ src/main/java/com/C4theBomb/datastructures/arraysandstrings/MatrixRotation.java
@@ -0,0 +1,29 @@
+package com.C4theBomb.datastructures.arraysandstrings;
+
+import java.lang.Math;
+
+public class MatrixRotation {
+    static int[][] rotateMatrix(int[][] matrix) {
+        int maxLength = matrix.length - 1;
+        int numbersInInner = matrix.length;
+
+        for (int d = 0; d < Math.ceil((m + 1)/ 2); d++) {
+            for (int i = 0; i < numbersInInner - 1; i++) {
+                // Stores top into variable tmp
+                int tmp = matrix[maxLength - (maxLength - d)][maxLength - (maxLength - (d + i))];
+                // Set top equal to left
+                matrix[maxLength - (maxLength - d)][maxLength - (maxLength - (d + i))] = matrix[maxLength - (d + i)][maxLength - (maxLength - d)];
+                // Set left equal to bottom
+                matrix[maxLength - (d + i)][maxLength - (maxLength - d)] = matrix[maxLength - d][maxLength - (d + i)];
+                // Set bottom equal to right
+                matrix[maxLength - d][maxLength - (d + i)] = matrix[maxLength - (maxLength- (d + i))][maxLength - d];
+                // Set right equal to tmp
+                matrix[maxLength - (maxLength- (d + i))][maxLength - d] = tmp;
+            }
+
+            numbersInInner -= 2;
+        }
+
+        return matrix;
+    }
+}

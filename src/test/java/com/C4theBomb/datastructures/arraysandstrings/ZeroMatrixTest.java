f165e50a8da99c24121fa7dc52718520a1675df7
C4 Patino <c4patino@gmail.com>
2021-12-13 20:17:28 -0600

    Edited display names

--- src/test/java/com/C4theBomb/datastructures/arraysandstrings/ZeroMatrixTest.java
+++ src/test/java/com/C4theBomb/datastructures/arraysandstrings/ZeroMatrixTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 
 public class ZeroMatrixTest {
     @Test
-    @DisplayName("")
+    @DisplayName("setZeroes - normal square input (3x3)")
     void test3By3() {
         int[][] initial = {{1, 2, 3}, {4, 0, 6}, {7, 8, 9}};
         int[][] complete = {{1, 0, 3}, {0, 0, 0}, {7, 0, 9}};
@@ -15,7 +15,7 @@ public class ZeroMatrixTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("setZeroes - normal uneven input (4x3)")
     void test4By3() {
         int[][] initial = {{0, 2, 3}, {4, 5, 6}, {7, 0, 9}, {10, 11, 12}};
         int[][] complete = {{0, 0, 0}, {0, 0, 6}, {0, 0, 0}, {0, 0, 12}};
@@ -24,14 +24,14 @@ public class ZeroMatrixTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("setZeroes - irregular input (all zeroes)")
     void testAllZeroes() {
         int[][] initial = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
         assertArrayEquals(initial, ZeroMatrix.setZeroes(initial));
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("setZeroes - irregular input (no zeroes)")
     void testNoZeroes() {
         int[][] initial = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         assertArrayEquals(initial, ZeroMatrix.setZeroes(initial));

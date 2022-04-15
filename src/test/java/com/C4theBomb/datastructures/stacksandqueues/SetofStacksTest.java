f517179a02308f1cc8a5482b575fe3ade9a40334
C4 Patino <c4patino@gmail.com>
2022-04-14 22:47:38 -0500

    Beginning of SetofStacks test suite

---
+++ src/test/java/com/C4theBomb/datastructures/stacksandqueues/SetofStacksTest.java
@@ -0,0 +1,25 @@
+package com.C4theBomb.datastructures.stacksandqueues;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import com.C4theBomb.datastructures.stacksandqueues.SetofStacks;
+
+public class SetofStacksTest {
+    @BeforeEach
+    void setupEach() {
+
+    }
+
+    @Test
+    @DisplayName("setOfStacks")
+    void testNormalMin() {
+    }
+
+    @Test
+    @DisplayName("setOfStacks - wrong min")
+    void testWrongMin() {
+    }
+}

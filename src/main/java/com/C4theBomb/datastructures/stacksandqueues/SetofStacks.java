0c70a401b54768512ba607a9f3cb85210fa074b7
C4 Patino <c4patino@gmail.com>
2022-04-14 22:47:22 -0500

    Beginning of SetofStacks test suite and solution

---
+++ src/main/java/com/C4theBomb/datastructures/stacksandqueues/SetofStacks.java
@@ -0,0 +1,92 @@
+package com.C4theBomb.datastructures.stacksandqueues;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+class Stack {
+    static public class StackNode {
+        private StackNode next;
+        int data;
+        StackNode min;
+
+        public StackNode(int data) {
+            this.data = data;
+        }
+    }
+
+    private StackNode top;
+    private int capacity;
+    private int height;
+
+    public Stack(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int pop() {
+        if (this.top == null)
+            throw new EmptyStackException();
+
+        this.height--;
+
+        int item = this.top.data;
+        this.top = this.top.next;
+
+        return item;
+    }
+
+    public void push(int item) {
+        if (this.isFull()) {
+            throw new StackOverflowError();
+        }
+
+        this.height++;
+
+        StackNode t = new StackNode(item);
+        t.next = this.top;
+        this.top = t;
+
+    }
+
+    public int peek() {
+        if (this.top == null)
+            throw new EmptyStackException();
+
+        return this.top.data;
+    }
+
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+
+    public boolean isFull() {
+        return this.height >= this.capacity;
+    }
+}
+
+public class SetofStacks {
+    ArrayList<Stack> set;
+    int stackCapacity;
+
+    SetofStacks(int stackCapacity) {
+        this.stackCapacity = stackCapacity;
+        this.set.add(new Stack(this.stackCapacity));
+    }
+
+    public void push(int item) {
+        if (this.set.get(-1).isFull()) {
+            set.add(new Stack(this.stackCapacity));
+        }
+
+        this.set.get(-1).push(item);
+    }
+
+    public int pop() {
+        int data = set.get(-1).pop();
+
+        if (this.set.get(-1).isEmpty()) {
+            set.remove(-1);
+        }
+
+        return data;
+    }
+}

027fb1abd5a94d83c5813981dca984a45b294255
C4 Patino <c4patino@gmail.com>
2022-06-06 15:17:22 -0500

    Created Formatting

--- src/main/java/com/C4theBomb/datastructures/treesandgraphs/BuildOrder.java
+++ src/main/java/com/C4theBomb/datastructures/treesandgraphs/BuildOrder.java
@@ -15,10 +15,32 @@ public class BuildOrder {
     }
 
     private static HashMap<String, ArrayList<String>> buildAjdList(ArrayList<Dependency> deps) {
+        HashMap<String, ArrayList<String>> depsList = new HashMap<>();
 
+        for (Dependency dep : deps) {
+            depsList.putIfAbsent(dep.prj, new ArrayList<String>());
+            depsList.get(dep.prj).add(dep.dep);
+        }
+
+        return depsList;
     }
 
     public static LinkedList<String> getBuildOrder(ArrayList<Dependency> deps) {
+        HashMap<String, ArrayList<String>> adjList = BuildOrder.buildAjdList(deps);
+        LinkedList<String> order = new LinkedList<>();
 
+        while ajdList not empty {
+            for key in ajdList keys {
+
+                if (key.value.length == 0) {
+                    for key in ajdList keys {
+                        pop key from key.value
+                    }
+                    remove key and add to buildOrder
+                }
+            }
+        }
+
+        return order;
     }
 }
